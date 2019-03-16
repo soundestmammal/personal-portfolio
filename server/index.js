cee18cb0cd76da871b04baf4f01b617c158bb8d2
Robert Checco <robert.checco@gmail.com>
2019-03-16 18:33:50 -0400

    Changes for Production

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("This is my portfolio rootpage");
-});
+app.use( express.static( `${__dirname}/../client/build` ) );
+
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
