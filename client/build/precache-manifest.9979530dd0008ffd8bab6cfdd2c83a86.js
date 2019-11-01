f79db4e3d0799e94269cef0a3ec82b56d31e2f80
Robert Checco <robert.checco@gmail.com>
2019-11-01 17:30:49 -0400

    Commit the build folder

---
+++ client/build/precache-manifest.9979530dd0008ffd8bab6cfdd2c83a86.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3704d8f50bfe47d0c076",
+    "url": "/static/js/main.3704d8f5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "fdeb06e132cd21d3b0807341a0b96a81",
+    "url": "/index.html"
+  }
+];
