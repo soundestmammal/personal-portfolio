aee1f9a5a4d82154e8b8ae0cf976540005b5ce5d
Robert Checco <robert.checco@gmail.com>
2019-11-02 15:56:00 -0400

    Add JSX markup for the Banner with minimal css to for layout

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -4,9 +4,30 @@ import '../styles/Banner.css';
 const Banner = () => {
     return (
         <div className="container">
-            <div>
-                <h2>I'm Rob Checco</h2>
-                <p>A Software Engineer</p>
+            {/*I need to create a few things 
+            Photo
+            Text column with three rows
+            Flush to the right a button
+            
+            */}
+            <div className="bannerPhoto">
+                <img src="https://source.unsplash.com/random/100x100" alt="something here"></img>
+            </div>
+
+            <div className="bannerDescription">
+                <h2>Rob Checco</h2>
+                <p>Software Engineer</p>
+                <div>
+                    <span>A</span>
+                    <span>B</span>
+                    <span>C</span>
+                    <span>D</span>
+                    <span>E</span>
+                </div>
+            </div>
+
+            <div className="bannerContactMe">
+                <button>This is a button!</button>
             </div>
 		</div>
     );
