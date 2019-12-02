3a596c38544f751152571ef5615bbcacebb2f357
Robert Checco <robert.checco@gmail.com>
2019-12-02 03:11:58 -0500

    Pass Styles prop to each square for color

--- client/src/components/ColorGame.js
+++ client/src/components/ColorGame.js
@@ -7,13 +7,31 @@ class ColorGame extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            colors: [],
             isOver: false,
             isAWinner: false,
             player1: true,
-            squares: Array(9).fill(null),
+        }
+        this.state.squares = new Array(9);
+        for(let i = 0; i < 9; i++) {
+            this.state.squares[i] = new Array(3);
+            for(let j = 0; j < this.state.squares[i].length; j++) {
+                this.state.squares[i][j] = this.generateRandomNumber();
+            }
         }
     }
+
+    // createDataStructure = () => {
+    //     const copySquares = this.state.squares.slice();
+    //     for(let i = 0; i < 9; i++) {
+    //         copySquares[i] = new Array(3);
+    //         for(let j = 0; j < copySquares[i].length; j++) {
+    //             copySquares[i][j] = this.generateRandomNumber();
+    //         }
+    //     }
+    //     this.setState( { squares: copySquares });
+    //     // console.log(copySquares);
+    //     console.log(this.state.squares);
+    // }
 
     generateRandomNumber = () => {
         const number = Math.floor(Math.random()*255);
@@ -27,10 +45,24 @@ class ColorGame extends Component {
             i++;
         }
     }
+
+    // populateSquaresWithColors = () => {
+    //     const copy = this.state.squares.splice();
+    //     for(let i = 0; i < 9; i++) {
+    //         for(let j = 0; j < 3; j++) {
+    //             let num = this.generateRandomNumber();
+    //             copy[i][j] = num;
+    //         };
+    //     };
+
+    //     this.setState( { squares: copy });
+        
+    // }
+
     renderSquare = (i) => {
         return (
             <Square
-                value={ this.state.squares[i] } 
+                styles={`rgb(${this.state.squares[i][0]}, ${this.state.squares[i][1]}, ${this.state.squares[i][2]})`}
                 onClick={ () => this.handleClick(i) }
             />
         );
@@ -61,7 +93,6 @@ class ColorGame extends Component {
             <div className="notOver">
                 <div className="status">Welcome to the color game</div>
                 {this.renderBoard()}
-                <button onClick={this.logToConsole}>Click to test random number thing</button>
                 <Link to="/"><button>Click this button to go back to the home page</button></Link>
             </div>
         );
