44b5e77b32a0cad77d294881cf007f8677f5a882
Robert Checco <robert.checco@gmail.com>
2019-12-08 16:12:15 -0500

    Push prod version for quick release

---
+++ client/build/precache-manifest.c647341da9342349ffc66e73a37f6078.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "df739972a630007a44b1",
+    "url": "/static/css/main.224981a4.chunk.css"
+  },
+  {
+    "revision": "df739972a630007a44b1",
+    "url": "/static/js/main.df739972.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "246559a92ff9604b71eb",
+    "url": "/static/css/2.30686459.chunk.css"
+  },
+  {
+    "revision": "246559a92ff9604b71eb",
+    "url": "/static/js/2.246559a9.chunk.js"
+  },
+  {
+    "revision": "62cae2f1499aa4478cc60355d6b1c617",
+    "url": "/static/media/minheap.62cae2f1.png"
+  },
+  {
+    "revision": "07258ad7a3897986aacb211d2c186126",
+    "url": "/static/media/maxheap.07258ad7.png"
+  },
+  {
+    "revision": "62745d6b46ec418ac2b7cc8e5441c73e",
+    "url": "/index.html"
+  }
+];
