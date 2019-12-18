29b1f2fb35fc65b828f1e9060c7480f4b0cdedf8
Robert Checco <robert.checco@gmail.com>
2019-12-18 15:16:33 -0500

    Add the new DS posts in the react router so they render

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
@@ -5,6 +6,9 @@ import Monty from './components/Monty';
 import Chat from './components/Chat';
 import Board from './components/Board';
 import ColorGame from './components/ColorGame';
+import DataStructures from './components/DataStructures';
+import Heapsort from './components/Heapsort';
+import BST from './components/BST';
 import App from './components/App';
 
 ReactDOM.render(
@@ -15,6 +19,9 @@ ReactDOM.render(
             <Route exact path="/colorgame" component={ColorGame} />
             <Route exact path="/monty" component={Monty} />
             <Route exact path="/chat" component={Chat} />
+            <Route exact path="/datastructures" component={DataStructures} />
+            <Route exact path="/datastructures/bst" component={BST} />
+            <Route exact path="/datastructures/heapsort" component={Heapsort} />
         </div>
     </BrowserRouter>,
     document.querySelector('#root')
