093396662451ab6ed17e080b607e7036ed9afbbe
Robert Checco <robert.checco@gmail.com>
2019-12-18 15:16:54 -0500

    Thinking about the banner that I am going to use - needs work

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,38 +1,16 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import '../styles/Banner.css';
+import dumbo from '../dumbo.jpg';
 
 const Banner = () => {
     return (
         <div className="container">
-            {/*I need to create a few things 
-            Photo
-            Text column with three rows
-            Flush to the right a button
-            
-            */}
-
-            <div className="leftBanner">
-                <div className="bannerPhoto">
-                    <img src="https://source.unsplash.com/random/100x100" alt="something here"></img>
+            <div className="background-image" style={{backgroundImage: dumbo}}>
+                <div className="frontPage">
+                    <div className="name">Rob Checco</div>
+                    <p className="aboutMeBanner">I am a New York City based software engineer and data science enthusiast.</p>
                 </div>
-
-                <div className="bannerDescription">
-                    <h2>Rob Checco</h2>
-                    <p>Software Engineer</p>
-                    <div>
-                        <span>A</span>
-                        <span>B</span>
-                        <span>C</span>
-                        <span>D</span>
-                        <span>E</span>
-                    </div>
-                </div>
-            </div>
-            
-
-            <div className="bannerContactMe">
-                <button>This is a button!</button>
             </div>
 		</div>
     );
