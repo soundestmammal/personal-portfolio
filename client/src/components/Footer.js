70b12658a2e6366b01bc2030a6cbe0cb101165b2
Rob <robert.checco@gmail.com>
2020-05-01 20:45:39 -0700

    Style Header and Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
     return(
-        <footer>
+        <footer className="footer">
             <a href="mainto:robemailhere" className="footer__link">robertchecco@gmail.com</a>
             <ul className="social-list">
-                <li className="social-list__item"><a href="github.com/soundestmammal">a</a></li>
-                <li className="social-list__item"><a href="github.com/soundestmammal">b</a></li>
-                <li className="social-list__item"><a href="github.com/soundestmammal">c</a></li>
-                <li className="social-list__item"><a href="github.com/soundestmammal">d</a></li>
+                <li className="social-list__item"><a href="github.com/soundestmammal" className="social-list__item"><FontAwesomeIcon icon={['fab', 'github']} /></a></li>
+                <li className="social-list__item"><a href="github.com/soundestmammal" className="social-list__item"><FontAwesomeIcon icon={['fab', 'kaggle']} /></a></li>
+                <li className="social-list__item"><a href="github.com/soundestmammal" className="social-list__item"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a></li>
+                <li className="social-list__item"><a href="github.com/soundestmammal" className="social-list__item"><FontAwesomeIcon icon={['fab', 'twitter']} /></a></li>
             </ul>
         </footer>
     );
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 
 import '../styles/style.css';
 
-const Header = () => {
-    return (
-        <header>
-            <div class="logo">
-                <img src="img/devpic.png" alt="" />
-            </div>
-            <button className="nav-toggle">
-                <span className="hamburger"></span>
-            </button>
-            <nav className="nav">
-                <ul className="nav__list">
-                    <li className="nav__item"><Link to="/" className="nav__link">Home</Link></li>
-                    <li className="nav__item"><Link to="/" className="nav__link">My Services</Link></li>
-                    <li className="nav__item"><Link to="/" className="nav__link">About me</Link></li>
-                    <li className="nav__item"><Link to="/" className="nav__link">My Work</Link></li>
-                </ul>
-            </nav>
-        </header>
-    )
+class Header extends Component {
+
+    // state = {
+    //     toggle: false
+    // };
+
+    // handleClick = (e) => {
+    //     e.preventDefault();
+    //     alert("Hell0!");
+    //     console.log("hello!!!")
+    // }
+
+    // showNavList = () => {
+
+    // }
+
+    render() {
+        return (
+            <header>
+                <div class="logo">
+                    <img src="img/devpic.png" alt="" />
+                </div>
+                <button className="nav-toggle" onClick={this.handleClick}>
+                    <span className="hamburger"></span>
+                </button>
+                
+                <nav className="nav">
+                    <ul className="nav__list">
+                        <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
+                        <li className="nav__item"><a href="#" className="nav__link">My Services</a></li>
+                        <li className="nav__item"><a href="#" className="nav__link">About Me</a></li>
+                        <li className="nav__item"><a href="#" className="nav__link">My Work</a></li>
+                    </ul>
+                </nav>
+            </header>
+        );
+    }
 }
 
 export default Header;
