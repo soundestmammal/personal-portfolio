54af86e8967fd23f2ed66201c9f8a85aaa122ef4
Rob <robert.checco@gmail.com>
2020-08-05 15:37:32 -0400

    Add titles and info for projects section

--- client/src/components/MyWork.js
+++ client/src/components/MyWork.js
@@ -24,25 +24,35 @@ const MyWork = () => {
 
             <div className="portfolio">
                 <a style={{cursor: 'pointer'}} onClick={() => openTabInNewTab("https://www.c19insights.io")} className="portfolio__item">
-                    <img src={img1} alt="First portfolio project" className="portfolio__img"/>
+                    <img src={img5} alt="First portfolio project" className="portfolio__img"/>
+                    <p className="portfolio__item-s">COVID-19 Insights</p>
+                    <p>An interactive dashboard that displays COVID-19 trends for the United States.</p>
                 </a>
             
-                <a href="/tictactoe" className="portfolio__item">
-                    <img src={img2} alt="First portfolio project" className="portfolio__img"/>
+                <a style={{cursor: 'pointer'}} onClick={() => openTabInNewTab("https://www.oasisglutenfree.com")} className="portfolio__item">
+                    <img src={img5} alt="First portfolio project" className="portfolio__img"/>
+                    <p className="portfolio__item-s">Oasis</p>
+                    <p>Oasis is a data-driven platform to locate gluten free restaurants in your community.</p>
                 </a>
 
            
-                <a href="/datastructures" className="portfolio__item">
-                    <img src={img3} alt="First portfolio project" className="portfolio__img"/>
+                <a href="/tictactoe" className="portfolio__item">
+                    <img src={img5} alt="First portfolio project" className="portfolio__img"/>
+                    <p className="portfolio__item-s">Tic Tac Toe</p>
+                    <p>Take turns playing tic tac toe with a friend.</p>
                 </a>
 
      
-                 <a href="/colorgame" className="portfolio__item">
-                    <img src={img4} alt="First portfolio project" className="portfolio__img"/>
+                 <a style={{cursor: 'pointer'}} onClick={() => openTabInNewTab("https://www.github.com/soundestmammal/senior_thesis")} className="portfolio__item">
+                    <img src={img5} alt="First portfolio project" className="portfolio__img"/>
+                    <p className="portfolio__item-s">Car Ownership</p>
+                    <p>A discrete choice model for car ownership NY Metro area.</p>
                 </a>
 
-                <a href="/monty" className="portfolio__item">
+                <a style={{cursor: 'pointer'}} onClick={() => openTabInNewTab("https://www.github.com/soundestmammal/mode_choice")} className="portfolio__item">
                     <img src={img5} alt="First portfolio project" className="portfolio__img"/>
+                    <p className="portfolio__item-s">Mode Choice</p>
+                    <p>A comparison of machine learning algorithms to predict how people get to work.</p>
                 </a>
 
                 {/*
