6ac51f6a9565bd43c0df33f54157c4871abfefe8
Rob <robert.checco@gmail.com>
2020-08-17 04:28:00 -0400

    Add colorful UI cards with project title, description, and links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-// import { BrowserRouter, Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-// import ReactGA from "react-ga";
-// import Banner from './Banner';
-// import Content from './Content';
-// import Toolbox from './Toolbox';
-// import { trackingId } from '../config/keys';
+
 import "../styles/style.css";
 
-import Header from './Header';
 import Introduction from './Introduction';
-import Services from './Services';
-import About from './About';
 import MyWork from './MyWork';
 import Footer from './Footer';
 library.add(fab);
-// import Main from './Main';
-// import Footer from './Footer';
-// import Dashboard from './Dashboard';
-// import UrbanEcon from './UrbanEcon';
-// import ChapterOne from './ChapterOne';
-
-// <Banner />
-// <Content />
-
 
 class App extends Component {
     constructor(props) {
@@ -34,33 +16,13 @@ class App extends Component {
             toggle: "wrapper"
         }
     }
-    // state = {
-    //     toggle: "wrapper"
-    // }
-
-    handleHamburger = () => {
-        if(this.state.toggle === "wrapper") {
-            this.setState({ toggle: "nav-open" });
-        } else {
-            this.setState({ toggle: "wrapper" });
-        }
-    }
 
     render() {
         return (
-            <div className={this.state.toggle}>
-                <Header handle={this.handleHamburger}/>
+            <div className="wrapper">
                 <Introduction />
-                {/* <Services /> */}
-                {/* <About /> */}
                 <MyWork />
                 <Footer />
-                {/* <Banner /> */}
-                {/* <Toolbox /> */}
-                {/* <Content /> */}
-                {/* <p>Hey! My name is Rob and this is my portfolio! Stay tuned to check out the latest from me!</p> */}
-                {/* <Link to="/tictactoe"><button>Click this button to see my tic tac toe project</button></Link> */}
-                {/* <Link to="/colorgame"><button>Click this button to see my color game project</button></Link> */}
             </div>
         )
     }
--- client/src/components/MyWork.js
+++ client/src/components/MyWork.js
@@ -1,16 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/style.css';
-import img1 from '../img/portfolio-01.jpg';
-import img2 from '../img/portfolio-02.jpg';
-import img3 from '../img/portfolio-03.jpg';
-import img4 from '../img/portfolio-04.jpg';
-import img5 from '../img/portfolio-05.jpg';
-// import img6 from '../img/portfolio-06.jpg';
-// import img7 from '../img/portfolio-07.jpg';
-// import img8 from '../img/portfolio-08.jpg';
-// import img9 from '../img/portfolio-09.jpg';
-// import img10 from '../img/portfolio-10.jpg';
 
 const MyWork = () => {
     function openTabInNewTab(url) {
@@ -20,59 +9,80 @@ const MyWork = () => {
     return (
         <section className="my-work">
             <h2 className="section__title section__title--work">My Projects</h2>
-            <p className="section__subtitle section__subtitle--work">A selection of my range of work</p>
-
-            <div className="portfolio">
-                <a style={{cursor: 'pointer'}} onClick={() => openTabInNewTab("https://www.c19insights.io")} className="portfolio__item">
-                    <img src={img5} alt="First portfolio project" className="portfolio__img"/>
-                    <p className="portfolio__item-s">COVID-19 Insights</p>
-                    <p>An interactive dashboard that displays COVID-19 trends for the United States.</p>
-                </a>
-            
-                <a style={{cursor: 'pointer'}} onClick={() => openTabInNewTab("https://www.oasisglutenfree.com")} className="portfolio__item">
-                    <img src={img5} alt="First portfolio project" className="portfolio__img"/>
-                    <p className="portfolio__item-s">Oasis</p>
-                    <p>Oasis is a data-driven platform to locate gluten free restaurants in your community.</p>
-                </a>
-
-           
-                <a href="/tictactoe" className="portfolio__item">
-                    <img src={img5} alt="First portfolio project" className="portfolio__img"/>
-                    <p className="portfolio__item-s">Tic Tac Toe</p>
-                    <p>Take turns playing tic tac toe with a friend.</p>
-                </a>
-
-     
-                 <a style={{cursor: 'pointer'}} onClick={() => openTabInNewTab("https://www.github.com/soundestmammal/senior_thesis")} className="portfolio__item">
-                    <img src={img5} alt="First portfolio project" className="portfolio__img"/>
-                    <p className="portfolio__item-s">Car Ownership</p>
-                    <p>A discrete choice model for car ownership NY Metro area.</p>
-                </a>
-
-                <a style={{cursor: 'pointer'}} onClick={() => openTabInNewTab("https://www.github.com/soundestmammal/mode_choice")} className="portfolio__item">
-                    <img src={img5} alt="First portfolio project" className="portfolio__img"/>
-                    <p className="portfolio__item-s">Mode Choice</p>
-                    <p>A comparison of machine learning algorithms to predict how people get to work.</p>
-                </a>
-
-                {/*
-                <a href="/" className="portfolio__item">
-                    <img src={img6} alt="First portfolio project" className="portfolio__img"/>
-                </a>
-
-                <a href="/" className="portfolio__item">
-                    <img src={img7} alt="First portfolio project" className="portfolio__img"/>
-                </a>
-                <a href="/" className="portfolio__item">
-                    <img src={img8} alt="First portfolio project" className="portfolio__img"/>
-                </a>
-                <a href="/" className="portfolio__item">
-                    <img src={img9} alt="First portfolio project" className="portfolio__img"/>
-                </a>
-                <a href="/" className="portfolio__item">
-                    <img src={img10} alt="First portfolio project" className="portfolio__img"/> 
-                </a>
-                */}
+            <p className="section__subtitle section__subtitle--work"></p>
+            <div className="portfolio_2">
+                <div className="card"  style={{ backgroundImage: "url(https://imgix.datadoghq.com/img/careers/careers_pattern_tech-solutions.png?auto=format)" }}>
+                    <div className="card-content">
+                        <div className="project-content">
+                            <h2 className="project-title">Oasis</h2>
+                            <p className="project-description">Oasis is a data driven service to identify restaurants that provide safe gluten-free options.</p>
+                        </div>
+                        <div className="button-container">
+                            <div className="project-button" onClick={() => openTabInNewTab("https://www.oasisglutenfree.com")}>View Project</div>
+                            <div className="project-button" onClick={() => openTabInNewTab("https://www.github.com/soundestmammal/oasis")}>View Code</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card" style={{ backgroundImage: "url(https://imgix.datadoghq.com/img/careers/careers_pattern_marketing.png?auto=format)" }}>
+                    <div className="card-content">
+                        <div className="project-content">
+                            <h2 className="project-title">COVID-19 Insights</h2>
+                            <p className="project-description">An interactive dashboard to visualize COVID-19 trends in the United States.</p>
+                        </div>
+                        <div className="button-container">
+                            <div className="project-button" onClick={() => openTabInNewTab("https://www.c19insights.io")}>View Project</div>
+                            <div className="project-button" onClick={() => openTabInNewTab("https://www.github.com/soundestmammal/covid19insights")}>View Code</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card" style={{ backgroundImage: "url(https://imgix.datadoghq.com/img/careers/careers_pattern_sales.png?auto=format)" }}>
+                    <div className="card-content">
+                            <div className="project-content">
+                                <h2 className="project-title">Vehicle Ownership</h2>
+                                <p className="project-description">Data science project that explores the determinants of vehicle ownership in the NY Metro Area using discrete choice modeling.</p>
+                            </div>
+                            <div className="button-container">
+                                <div className="project-button" onClick={() => openTabInNewTab("https://github.com/soundestmammal/Senior_Thesis/blob/master/Capstone_Project_Version_0-2.pdf")}>View Project</div>
+                                <div className="project-button" onClick={() => openTabInNewTab("https://www.github.com/soundestmammal/Senior_Thesis")}>View Code</div>
+                            </div>
+                    </div>
+                </div>                
+                <div className="card" style={{ backgroundImage: "url(https://imgix.datadoghq.com/img/careers/careers_pattern_GA.png?auto=format)" }}>
+                    <div className="card-content">
+                            <div className="project-content">
+                                <h2 className="project-title">Mode Choice</h2>
+                                <p className="project-description">A comparision of three different models Logisic Regression, Support Vector Machine, and Neural Network to predict how people travel to work.</p>
+                            </div>
+                            <div className="button-container">
+                                <div className="project-button" onClick={() => openTabInNewTab("https://github.com/soundestmammal/Mode_Choice/blob/master/Mode_Choice_Paper.pdf")}>View Project</div>
+                                <div className="project-button" onClick={() => openTabInNewTab("https://github.com/soundestmammal/Mode_Choice")}>View Code</div>
+                            </div>
+                    </div>
+                </div>                
+                <div className="card" style={{ backgroundImage: "url(https://imgix.datadoghq.com/img/careers/careers_pattern_OS.png?auto=format)" }}>
+                    <div className="card-content">
+                            <div className="project-content">
+                                <h2 className="project-title">Dynamic Vehicle Model</h2>
+                                <p className="project-description">Vehicle accepts throttle input and steps through longitudinal dynamic equations.</p>
+                            </div>
+                            <div className="button-container">
+                                <div className="project-button" onClick={() => openTabInNewTab("https://github.com/soundestmammal/dynamic-vehicle-modeling")}>View Project</div>
+                                <div className="project-button" onClick={() => openTabInNewTab("https://github.com/soundestmammal/dynamic-vehicle-modeling")}>View Code</div>
+                            </div>
+                    </div>
+                </div>                
+                <div className="card" style={{ backgroundImage: "url(https://imgix.datadoghq.com/img/careers/careers_pattern_engineering.png?auto=format)" }}>
+                    <div className="card-content">
+                            <div className="project-content">
+                                <h2 className="project-title">NYC Real Estate</h2>
+                                <p className="project-description">How much more money are people willing to pay to be near a subway stop?</p>
+                            </div>
+                            <div className="button-container">
+                                <div className="project-button" onClick={() => openTabInNewTab("https://github.com/soundestmammal/econometrics_spring_2018/blob/master/Econometrics_Paper_V0-02.pdf")}>View Project</div>
+                                <div className="project-button" onClick={() => openTabInNewTab("https://github.com/soundestmammal/econometrics_spring_2018")}>View Code</div>
+                            </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
