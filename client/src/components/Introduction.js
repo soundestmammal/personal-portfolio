d9a3b6fbc9af190bef006dad3ec8487eb7f51a07
Rob <robert.checco@gmail.com>
2020-09-28 21:01:46 -0400

    Remove picture from introduction

--- client/src/components/Introduction.js
+++ client/src/components/Introduction.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import Rob from '../rob3.jpg';
+import React from "react";
+import Rob from "../rob3.jpg";
 
 const Introduction = () => {
-    return (
-        <div className="intro">
-            <h1 className="section__title section__title--intro">
-                Hi, I am <strong>Robert Checco</strong>
-            </h1>
-            <p className="section__subtitle section__subtitle--intro">software engineer</p>
-            <img src={Rob} alt="rob smiling" className="intro__img"/>
-        </div>
-    );
-}
+  return (
+    <div className="intro">
+      <div className="intro-landing">
+        <h1 className="section__title section__title--intro">
+          Hi, I am <strong>Robert Checco</strong>
+        </h1>
+        <p className="section__subtitle section__subtitle--intro">
+          I am a full stack engineer.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Introduction;
